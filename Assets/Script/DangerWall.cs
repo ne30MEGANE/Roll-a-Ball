8f6ec387f4ccb036e0e43bca6ab1176c321ed682
Takahashi <ne300229@senshu-u.jp>
2019-03-03 06:18:04 +0900

    こみってぃんぐ。

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour {
+    private void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+        if(count == 0) {
+            winnerLabelObject.SetActive(true);
+        }
+    }
+}
